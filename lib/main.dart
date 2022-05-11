16ab48a5b94824d69893cc94d8e966369bd13022
Thomas Andrew Zaragoza <thomasandrewzaragoza@gmail.com>
2022-05-12 01:21:22 +0800

    chore: change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Login Screen',
       theme: ThemeData.light(),
       home: const Login(),
     );
